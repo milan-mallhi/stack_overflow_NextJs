1ca3eee4c69a981a71f8ca8cb57471b3bd2eb0a5
Digilearning000 <digilearning000@gmail.com>
2024-01-02 21:45:01 +0530

    trying to fix error

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -18,8 +18,6 @@ const LeftSidebar = () => {
       <div className=" flex flex-1 flex-col gap-6">
         {sidebarLinks.map((item) => {
           const isActive =
-            // (pathname.includes(item.route) && item.route.length > 1) ||
-            // pathname === item.route;
             (pathname.includes(item.route) && item.route.length > 1) ||
             pathname === item.route;
 
