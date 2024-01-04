47e013aa397cf58837814ef9a4ad8a1133a254e4
Digilearning000 <digilearning000@gmail.com>
2024-01-04 19:52:23 +0530

    Added community page

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -5,6 +5,7 @@ import { connectToDatabase } from "../mongoose";
 import {
   CreateUserParams,
   DeleteUserParams,
+  GetAllUsersParams,
   UpdateUserParams,
 } from "./shared.types";
 import { revalidatePath } from "next/cache";
@@ -86,3 +87,28 @@ export async function deleteUser(params: DeleteUserParams) {
     throw error;
   }
 }
+
+export async function getAllUsers(params: GetAllUsersParams) {
+  try {
+    connectToDatabase();
+
+    // const { page = 1, pageSize = 20, filter, searchQuery } = params;
+
+    const users = await User.find({});
+
+    return { users };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+// export async function getAllUsers(params: GetAllUsersParams) {
+//   try {
+//     connectToDatabase();
+
+//   } catch (error) {
+//     console.log(error);
+//     throw error;
+//   }
+// }
