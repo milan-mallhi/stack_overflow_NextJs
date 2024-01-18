a14e03bf1cdc75c52e5130f383135217be4e59e2
Digilearning000 <digilearning000@gmail.com>
2024-01-18 15:50:18 +0530

    top result finished

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -209,3 +209,16 @@ export async function editQuestion(params: EditQuestionParams) {
     throw error;
   }
 }
+
+export async function getHotQuestions() {
+  try {
+    connectToDatabase();
+
+    const hotQuestions = await Question.find({}).sort({ upvotes: -1 }).limit(5);
+
+    return hotQuestions;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
